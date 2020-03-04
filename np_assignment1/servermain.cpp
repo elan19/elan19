3ce829e8aa6e26a3df22e24967d2d85d40f420b6
Elias Andersson <elias.pilias@hotmail.com>
2020-03-04 15:04:42 +0100

    skapar socket

--- np_assignment1/servermain.cpp
+++ np_assignment1/servermain.cpp
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 /* You will to add includes here */
+#include <sys/types.h>
+#include <Windows.h>
+#include <sys/>
+#include <WinSock2.h>
 
 
 // Included to get the support library
@@ -14,6 +18,7 @@ using namespace std;
 int main(int argc, char *argv[]){
   
   /* Do more magic */
-  
+    int socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);         
+
 
 }
